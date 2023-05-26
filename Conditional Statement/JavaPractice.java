a8573ebeb0d128356c3200fbe740fb0b6dbfefd6
Arbazkhan-cs <121720813+Arbazkhan-cs@users.noreply.github.com>
2023-05-26 11:17:04 +0530

    Second Commit

---
+++ Conditional Statement/JavaPractice.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class JavaPractice {
+    public static void main(String args[]){
+
+        // // Find that the given number is possitive or negative.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter The Number");
+        // int num = sc.nextInt();
+
+        // String type = num>=0 ? "POSSITIVE":"NEGATIVE";
+        // System.out.println("The Number Is = "+type);
+
+
+
+        // // If temperature is greater than 100 print fever
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Temperature in Ferhenheight: ");
+        // double temp = sc.nextDouble();
+
+        // if(temp>=100){
+        //     System.out.println("You have a fever");
+        // } else{
+        //     System.out.println("You Don't Have Fever");
+        // }
+
+
+
+        // // print week name according to the given number by using switch case.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter The Number Form [1 to 7]");
+        // int week = sc.nextInt();
+
+        // switch(week){
+        //     case 1: System.out.println("It's Monday");
+        //             break;
+        //     case 2: System.out.println("It's Tuesday");
+        //             break;
+        //     case 3: System.out.println("It's Wednesday");
+        //             break;
+        //     case 4: System.out.println("It's Thursday");
+        //             break;
+        //     case 5: System.out.println("It's Friday");
+        //             break;
+        //     case 6: System.out.println("It's Saturday");
+        //             break;
+        //     case 7: System.out.println("It's Sunday");
+        //             break;
+        //     default: System.out.println("Invalid Input");
+        // }
+        
+        
+
+
+        // //  // Whather a given year is leap year or not
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter The Year");
+        // int year = sc.nextInt();
+
+        // boolean x = (year%4) == 0;
+        // boolean y = (year%100) != 0;
+        // boolean z = ((year%100) == 0) && ((year%400) == 0); 
+
+        // if(x && (y || z)){
+        //     System.out.println("Leap Year");
+        // } else{
+        //     System.out.println("Not a Leap year");
+        // }
+    }
+}
---
+++ Conditional Statement/javaProg.java
@@ -0,0 +1,127 @@
+import java.util.*;
+public class javaProg{
+    public static void main(String arg[]){
+        // Scanner scan = new Scanner(System.in);
+
+        // System.out.print("Enter Your Age: ");
+        // int age = scan.nextInt();
+
+        // if(age>=18){
+        //     System.out.println("You are adult, you can drive");
+        // }else{
+        //     System.out.println("Your are not adult!, you can't drive");
+        // }
+
+
+
+        // Find the largest of two numbers
+        // Scanner scan = new Scanner(System.in);
+        // System.out.print("Enter Two Numbers: ");
+        // int num1 = scan.nextInt();
+        // int num2 = scan.nextInt();
+
+        // if(num1>num2){
+        //     System.out.println(num1+" is begger");
+        // }
+        // else{
+        //     System.out.println(num2+" is begger");
+        // }
+
+
+
+
+
+        // // Find that number is odd or even;
+        // Scanner scan = new Scanner(System.in);
+        // System.out.print("Enter the number: ");
+        // int num = scan.nextInt();
+
+        // if(num%2 == 0){
+        //     System.out.println("Number is even");
+        // }else{
+        //     System.out.println("Numbe is odd");
+        // }
+
+
+
+    //     // Income tax calculator
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter Your Income: ");    
+    //     int income = sc.nextInt();
+
+    //     int tax=0;
+    //     if(income<500000){
+    //         tax=0;
+    //     }
+    //     else if(income>500000 && income<1000000){
+    //         tax = (income*20)/100;
+    //     }
+    //     else{
+    //         tax = (income*30)/100;
+    //     }
+    //     System.out.println("Your Tax is = "+tax);
+    //     System.out.println("Your final income is = "+(income-tax));
+
+        
+
+
+        // // Find largest number among 3 numbers
+        // Scanner sc= new Scanner(System.in);
+        // System.out.println("Enter Three numbers:");
+        // int num1 = sc.nextInt();
+        // int num2 = sc.nextInt();
+        // int num3 = sc.nextInt();
+
+        // if(num1>num2 && num1>num3){
+        //     System.out.println(num1+" is LARGEST");
+        // }
+        // else if(num2>num3){
+        //     System.out.println(num2+" is LARGEST");
+        // }
+        // else{
+        //     System.out.println(num3+" IS LARGEST");
+        // }
+
+
+        // // Pass or Fail
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Your Marks: ");
+        // int num = sc.nextInt();
+        
+        // String result = (num>=33)? "Pass": "Fail";
+        // System.out.println(result);
+
+
+
+
+
+        // // Creating Calculator
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("1). Addition ");
+        // System.out.println("2). Subtraction ");
+        // System.out.println("3). Multiplication ");
+        // System.out.println("4). Divition ");
+        // System.out.println("5). Modulo ");
+        // System.out.print("Chose one option [1, 2, 3, 4, 5]: ");
+        // int choice = sc.nextInt();
+
+        // System.out.print("Enter First Number: ");
+        // int num1 = sc.nextInt();
+        // System.out.print("Enter Second Number: ");
+        // int num2 = sc.nextInt();
+
+        // switch(choice){
+        //     case 1: System.out.println("Addition of num1+num2 = "+(num1+num2));
+        //                     break;
+        //     case 2: System.out.println("Subtraction of num1-num2 =  "+(num1-num2));
+        //                     break;
+        //     case 3: System.out.println("Multiplication of num1*num2 = "+(num1*num2));
+        //                     break;
+        //     case 4: System.out.println("Divition of num1/num2 = "+(num1/num2));
+        //                     break;
+        //     case 5: System.out.println("Modulo of num1%num2 = "+(num1%num2));
+        //                     break;
+        //     default: System.out.println("Invalid Choice or numbers!!!");
+        // }
+    }
+}

e5aa0c46ea938d96422da02078636dd3fbde79ce
Arbazkhan-cs <121720813+Arbazkhan-cs@users.noreply.github.com>
2024-01-27 08:05:55 +0530

    Updating Revsision Backtracking

--- Practice/Backtracking.java
+++ Practice/Backtracking.java
@@ -27,9 +27,62 @@ class Backtracking{
             PrintPermutation(Newstr, perm+c);
         }
     }
+    // ================================================= NQueens =====================================
+    public static boolean isSafe(int[][] board, int row, int col){
+        for(int i = row-1; i>=0; i--){
+            if(board[i][col] == 1){
+                return false;
+            }
+        }
+        // left upper diagonal
+        for(int i = row-1, j = col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j] == 1){
+                return false;
+            }
+        }
+        // right upper diagonal
+        for(int i = row-1, j = col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j] == 1){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void NQueens(int[][] board, int row){
+        if(row == board.length){
+            printChess(board);
+            return;
+        }
+        for(int j = 0; j<board.length; j++){
+            if(isSafe(board, row, j)){
+                board[row][j] = 1;
+                NQueens(board, row+1);
+                board[row][j] = 0;
+            }
+        }
+    }
+    public static void printChess(int[][] board){
+        System.err.println("\n============= board =============");
+        for(int i=0; i<board.length; i++){
+            for(int j = 0; j<board[0].length; j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.err.println();
+        }
+    }    
     public static void main(String[] args) {
         System.err.println("Hello World");
         PrintSubSets("abc", 0, "");
         PrintPermutation("abc", "");
+
+        int N = 4;
+        int[][] board = new int[N][N];
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                board[i][j] = 0;
+            }
+        }
+        NQueens(board, 0);
+        System.err.println("\nDone");
     }
 }

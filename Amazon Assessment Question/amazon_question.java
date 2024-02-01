0b8a3b323a716ed84a7aa0df8489a018a6a001ef
Arbazkhan-cs <121720813+Arbazkhan-cs@users.noreply.github.com>
2024-02-01 10:20:33 +0530

    Adding Greedy Algorithm

--- Amazon Assessment Question/amazon_question.java
+++ Amazon Assessment Question/amazon_question.java
@@ -10,7 +10,55 @@ maxSpread = 2
 
 The trainee in any class must within maxSpeed = 2 levels of each other. In this case, one optimal grouping is (1, 3), (4, 4), and(7). Another possible grouping is (1),(3,4,4),(7). There is no way to form than 3 classes. */
 
-public class AmazonQuestion {
+class amazon_question {
+    public static int maxGroup(List<Integer> levels, int maxSpread){
+        Collections.sort(levels);
+        int count = 0;
+        int i = 0;
+        while(i<levels.size()){
+            int j=i+1;
+            while(j<levels.size() && Math.abs(levels.get(i)-levels.get(j))<=maxSpread)
+                j++ ;
+
+            count += 1;
+            i=j;
+        }
+        
+        return count;   
+    }
+    public static int maxGroupIUsed(List<Integer>  levels, int maxSpread){
+        int count = 0;
+        int[] visited = new int[levels.size()];
+
+        for(int i = 0; i<levels.size(); i++){
+            Stack<Integer> st = new Stack<>();
+            st.push(levels.get(i));
+            int flag = 0;
+            for(int j = 1; j<levels.size(); j++){
+                if(Math.abs(levels.get(i) - levels.get(j)) <= maxSpread
+                    && Math.abs(st.peek() - levels.get(j))  <= maxSpread
+                    &&  visited[j] == 0){
+                        st.push(levels.get(j));
+                        visited[j] = 1;
+                        flag = 1;
+                }
+            }
+
+            if(flag == 1){
+                count += 1;
+            } else{
+                if(st.size() == 1){
+                    int num = st.pop();
+                    int idx = levels.indexOf(num);
+                    if(visited[idx] == 0){
+                        count+=1;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
     public static void main(String args[]) {
         // Inputs
         List<Integer> levels = new ArrayList<>();
@@ -19,77 +67,11 @@ public class AmazonQuestion {
         levels.add(7);
         levels.add(3);
         levels.add(4);
-        int maxDist = 2;
+        int maxSpread = 2;
 
-        // Solution
-        int count = 0;
-        boolean[] visited = new boolean[levels.size()];
-
-        for (int i = 0; i < levels.size(); i++) {
-            if (!visited[i]) {
-                count++;
-                int currentSkill = levels.get(i);
-                visited[i] = true; // Mark current trainee as visited
-                // Check for other trainees who can form a group with the current trainee
-                for (int j = i + 1; j < levels.size(); j++) {
-                    if (!visited[j] && Math.abs(currentSkill - levels.get(j)) <= maxDist) {
-                        visited[j] = true; // Mark the trainee as visited
-                    }
-                }
-            }
-        }
-
-        System.out.println(count);
+        // Solution - greedy method
+        System.err.println(maxGroup(levels, maxSpread));
+        // Soltuion - i used
+        System.err.println(maxGroupIUsed(levels, maxSpread));
     }
 }
-
-// My approach ==============================================================================
-// import java.util.*;
-
-// public class amazon_question {
-//     public static void main(String args[]){
-//         // Inputs =============================================
-//         List<Integer> levels = new ArrayList<>();
-//         levels.add(1);
-//         levels.add(4);
-//         levels.add(7);
-//         levels.add(3);
-//         levels.add(4);
-//         int maxDist = 2;
-
-//         // Solution ============================================
-//         int count = 0;
-//         int[] visited = new int[levels.size()];
-
-//         for(int i = 0; i<levels.size(); i++){
-//             Stack<Integer> st = new Stack<>();
-//             st.push(levels.get(i));
-//             int flag = 0;
-//             for(int j = 1; j<levels.size(); j++){
-//                 if(Math.abs(levels.get(i) - levels.get(j)) <= maxDist
-//                     && Math.abs(st.peek() - levels.get(j))  <= maxDist
-//                     &&  visited[j] == 0){
-//                         st.push(levels.get(j));
-//                         visited[j] = 1;
-//                         flag = 1;
-//                 }
-//             }
-
-//             if(flag == 1){
-//                 count += 1;
-//             } else{
-//                 if(st.size() == 1){
-//                     int num = st.pop();
-//                     int idx = levels.indexOf(num);
-//                     if(visited[idx] == 0){
-//                         count+=1;
-//                     }
-//                 }
-//             }
-//         }
-
-//         System.err.println(count);
-
-//     }
-// }
-

6a087956d5e121c94b36f9e7104c8d28bcb8c2d9
Arbazkhan-cs <121720813+Arbazkhan-cs@users.noreply.github.com>
2024-02-04 20:29:19 +0530

    Updating files

--- Practice/LinkedList.java
+++ Practice/LinkedList.java
@@ -143,6 +143,115 @@ public class LinkedList {
     public int recusiveSearch(int key){
         return helper(head, key);
     }
+
+    public void reverseList(){
+        if(size == 0 || size == 1){
+            return;
+        }
+        Node currNode = tail = head;
+        Node prevNode = null;
+        while(currNode != null){
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        };
+        head = prevNode;
+        return;
+    }
+    public int removeFromEnd(int idx){
+        // calculating Size of the linked list
+        int Size = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            Size++;
+        }
+
+        // if index is less than 0 or more than the ll size
+        if(idx <= 0 || idx > Size){
+            System.err.println("Node Out Of Range!");
+            return Integer.MIN_VALUE;
+        }
+
+        // if idx is equal to size (1st Node from head)
+        if(idx == Size){
+            int data = head.data;
+            head = head.next;
+            return data;
+        }
+
+        //  Main Logic to remove from midle
+        temp = head;
+        int idxFromStart = Size - idx - 1; // -1 , temp to the previous node, which we want to delete
+        while(idxFromStart != 0){
+            temp = temp.next;
+            idxFromStart--;
+        }
+        int data = temp.next.data;
+        temp.next = temp.next.next;
+        if(idx == 1){
+            tail = temp;
+        }
+        size--;
+        return data;
+    }
+
+    public boolean isPalindromeByString(){ // O(n) - time, O(n) - space
+        if(head == null || head.next == null){
+            return true;
+        }
+        StringBuilder st = new StringBuilder();
+        Node temp = head;
+        while(temp != null){
+            st.append(temp.data);
+            temp = temp.next;
+        }
+
+        int low = 0;
+        int high = st.length()-1;
+        while(low <=  high){
+            if(st.charAt(low) != st.charAt(high)){
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(){ // O(n) - time, O(1) - space
+        if(head == null || head.next == null){
+            return true;
+        }
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node middleNode = slow;
+        Node prevNode = null;
+        while(middleNode != null){
+            Node nextNode = middleNode.next;
+            middleNode.next = prevNode;
+            prevNode = middleNode;
+            middleNode = nextNode;
+        }
+
+        Node temp1 = head;
+        Node temp2 = prevNode;
+        while(temp1 != null && temp2 != null){
+            if(temp1.data != temp2.data){
+                return false;
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        return true;
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         // ll.head = new Node(10);
@@ -156,27 +265,43 @@ public class LinkedList {
         // System.err.println(ll.head.next.data);
         // System.err.println(ll.head.next.next.data);
 
-        ll.addFirst(1);
-        ll.addFirst(2);
-        ll.addFirst(3);
-        ll.addLast(4);
-        ll.addLast(5);
-        ll.addLast(6);
-        ll.display();
+        // ll.addFirst(1);
+        // ll.addFirst(2);
+        // ll.addFirst(3);
+        // ll.addLast(4);
+        // ll.addLast(5);
+        // ll.addLast(6);
+        // ll.display();
 
-        ll.removeFirst();
-        ll.display();
-        ll.removeLast();
-        ll.display();
+        // ll.removeFirst();
+        // ll.display();
+        // ll.removeLast();
+        // ll.display();
 
-        ll.add(0, 7);
-        ll.add(1, 8);
-        ll.add(3, 9);
-        ll.add(6, 0);
-        ll.display();
-        System.err.println("LinkedList Size = "+ll.size);
-        System.err.println("1 is at Index = " + ll.iterativeSearch(1));
-        System.err.println("1 is at Index = " + ll.recusiveSearch(1));
+        // ll.add(0, 7);
+        // ll.add(1, 8);
+        // ll.add(3, 9);
+        // ll.add(6, 0);
+        // ll.display();
+        // System.err.println("LinkedList Size = "+ll.size);
+        // System.err.println("1 is at Index = " + ll.iterativeSearch(1));
+        // System.err.println("1 is at Index = " + ll.recusiveSearch(1));
 
+        // ll.reverseList();
+        // ll.display();
+        // System.err.println("Head = "+ll.head.data + ", Tail = "+ll.tail.data);
+        
+
+        // ll.removeFromEnd(ll.size-1);
+        // ll.display();
+        // System.err.println("Head = "+ll.head.data + ", Tail = "+ll.tail.data);
+
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(2);
+        ll.addLast(1);
+        System.err.println("Is Palindorme: "  + ll.isPalindromeByString());
+        System.err.println("Is Palindorme: "  + ll.isPalindrome());
     }
 }

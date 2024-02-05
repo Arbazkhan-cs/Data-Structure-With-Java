f3456accd47c56861f9e4ad661aac6b9aaa4d43c
Arbazkhan-cs <121720813+Arbazkhan-cs@users.noreply.github.com>
2024-02-05 09:31:26 +0530

    Updating LinkedList

--- Practice/Linked_List.java
+++ Practice/Linked_List.java
@@ -1,4 +1,5 @@
-public class LinkedList {
+import java.util.LinkedList;
+public class Linked_List {
     static class Node{
         int data;
         Node next;
@@ -252,8 +253,94 @@ public class LinkedList {
 
         return true;
     }
+
+    public boolean detectCycle(){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void removeCycle(){
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle == false){
+            return;
+        }
+        slow = head;
+        Node prev = null;
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        prev.next = null;
+        return;
+    }
+
+    public Node mergeSort(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node RightHalf = slow.next;
+        slow.next = null;
+        Node leftHead = mergeSort(head);
+        Node rightHead = mergeSort(RightHalf);
+
+        return Merge(leftHead, rightHead);
+    }
+    public Node Merge(Node leftHead, Node rightHead){
+        Node mergeLL = new Node(-1);
+        Node temp = mergeLL;
+
+        while(leftHead != null && rightHead != null){
+            if(leftHead.data <= rightHead.data){
+                temp.next = leftHead;
+                leftHead = leftHead.next;
+            } else {
+                temp.next = rightHead;
+                rightHead = rightHead.next;
+            }
+            temp = temp.next;
+        }
+
+        while(leftHead != null){
+            temp.next = leftHead;
+            leftHead = leftHead.next;
+            temp = temp.next;
+        }
+        
+        while(rightHead != null){
+            temp.next = rightHead;
+            rightHead = rightHead.next;
+            temp = temp.next;
+        }
+
+        return mergeLL.next;
+    }
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
+        // Linked_List ll = new Linked_List();
         // ll.head = new Node(10);
         // ll.tail = ll.head;
         // ll.head.data = 15;
@@ -297,11 +384,52 @@ public class LinkedList {
         // System.err.println("Head = "+ll.head.data + ", Tail = "+ll.tail.data);
 
 
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addLast(2);
-        ll.addLast(1);
-        System.err.println("Is Palindorme: "  + ll.isPalindromeByString());
-        System.err.println("Is Palindorme: "  + ll.isPalindrome());
+        // ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addLast(2);
+        // ll.addLast(1);
+        // System.err.println("Is Palindorme: "  + ll.isPalindromeByString());
+        // System.err.println("Is Palindorme: "  + ll.isPalindrome());
+
+
+        // Node newnode1 = new Node(1);
+        // Node newnode2 = new Node(2);
+        // Node newnode3 = new Node(3);
+        // Node newnode4 = new Node(4);
+        // Node newnode5 = new Node(5);
+        // Node newnode6 = new Node(6);
+        // ll.head = newnode1;
+        // newnode1.next = newnode2;
+        // newnode2.next = newnode3;
+        // newnode3.next = newnode4;
+        // newnode4.next = newnode5;
+        // newnode5.next = newnode6;
+        // newnode6.next = newnode3;
+
+
+        // System.err.println("Detect Cycle: "  + ll.detectCycle());
+        // ll.removeCycle();
+        // System.err.println("Detect Cycle: "  + ll.detectCycle());
+        // ll.display();
+
+
+        // Java Collection Framework - Linked List======================================
+        // LinkedList<Integer> ll = new LinkedList<>();
+        // ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addFirst(3);
+        // ll.addFirst(4);
+        // System.err.println(ll);
+
+        Linked_List ll = new Linked_List();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.display();
+        ll.head = ll.mergeSort(ll.head);
+        ll.display();
+        
     }
-}
+}   

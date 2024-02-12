f98c3ec29b4c9a0d14babefb73a0e1881671fd71
Arbazkhan-cs <121720813+Arbazkhan-cs@users.noreply.github.com>
2024-02-12 08:47:11 +0530

    Uploading practice binary tree

---
+++ Practice/BinaryTree.java
@@ -0,0 +1,134 @@
+import java.util.*;
+public class BinaryTree{
+    static class Node{
+        int data;
+        Node leftChild;
+        Node rightChild;
+        public Node(int data){
+            this.data = data;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+    }
+    static class binaryTree{
+        int idx = -1;
+        public Node buildTreePreorder(int[] nodes){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.leftChild = buildTreePreorder(nodes);
+            newNode.rightChild = buildTreePreorder(nodes);
+            return newNode;
+        }
+        public void printPreorder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data + " ");
+            printPreorder(root.leftChild);
+            printPreorder(root.rightChild);
+        }
+        public void printInorder(Node root){
+            if(root == null){
+                return;
+            }
+            printInorder(root.leftChild);
+            System.err.print(root.data + " ");
+            printInorder(root.rightChild);
+        }
+        public void printPostorder(Node root){
+            if(root == null){
+                return;
+            }
+            printPostorder(root.leftChild);
+            printPostorder(root.rightChild);
+            System.err.print(root.data + " ");
+        }
+        public void printLevelOrder(Node root){
+            if(root == null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode == null){
+                    System.err.println();
+                    if(q.isEmpty()){
+                        break;
+                    } else{
+                        q.add(null);
+                    }
+                } else{
+                    System.err.print(currNode.data + " ");
+                    if(currNode.leftChild != null){
+                        q.add(currNode.leftChild);
+                    } 
+                    if(currNode.rightChild != null){
+                        q.add(currNode.rightChild);
+                    }
+                }
+            }
+        }
+        public int heightOfTree(Node root){
+            if(root == null){
+                return 0;
+            }
+            int leftHeight = heightOfTree(root.leftChild);
+            int rightChild = heightOfTree(root.rightChild);
+
+            return Math.max(leftHeight, rightChild) + 1;
+        }
+        public int countNodes(Node root){
+            if(root == null){
+                return 0;
+            }
+
+            int countLeft = countNodes(root.leftChild);
+            int countRight = countNodes(root.rightChild);
+
+            return countLeft + countRight + 1;
+        }
+        public int sumOfNodes(Node root){
+            if(root == null){
+                return 0;
+            }
+            int sumLeft = sumOfNodes(root.leftChild);
+            int sumRight = sumOfNodes(root.rightChild);
+
+            return sumLeft + sumRight + root.data;
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        binaryTree tree = new binaryTree();
+        Node root = tree.buildTreePreorder(nodes);
+        System.err.print("Preorder = [ ");
+        tree.printPreorder(root);
+        System.err.println("]");
+
+        System.err.print("Inorder = [ ");
+        tree.printInorder(root);
+        System.err.println("]");
+
+        System.err.print("Postorder = [ ");
+        tree.printPostorder(root);
+        System.err.println("]");
+
+        System.err.println("Levelorder = [ ");
+        tree.printLevelOrder(root);
+        System.err.println("]");
+
+        System.err.println("Height of the tree = "+ tree.heightOfTree(root));
+        System.err.println("Number of nodes = "+ tree.countNodes(root));
+        System.err.println("Sum of nodes = "+ tree.sumOfNodes(root));
+
+    }
+}
